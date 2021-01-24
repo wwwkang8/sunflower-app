9f03a59a6a6257e60435cd9acc463286be3c362e
wwwkang8 <wwwkang8@gmail.com>
2021-01-24 22:49:42 +0900

    #10.1 Preloading Assets

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Asset } from 'expo-asset';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [loaded, setLoaded] = useState(false);
+  const preLoad = async () => {
+    try{
+      await Font.loadAsync({
+        ...Ionicons.font
+      });
+      await Asset.loadAsync([require("./assets/instagram_logo.png")]);
+      setLoaded(true);
+    }catch(e){
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  return loaded ? (
+    <View>
+      <Text>Open up App  . start working with your app</Text>
     </View>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
