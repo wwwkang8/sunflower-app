dbf3adaf57d73f3c4b244fa55573e665a9817c9a
wwwkang8 <wwwkang8@gmail.com>
2021-02-03 23:48:59 +0900

    fix: #11.2 Tabs Navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
+import { View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from "../screens/Home";
@@ -9,17 +10,29 @@ import Search from "../screens/Search";
 
 const Tab = createBottomTabNavigator();
 
-const TabNavigation = (
-
+export default () => {
+    return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Profile" component={Profile} />
-                <Tab.Screen name="Search" component={Search} />
-            </Tab.Navigator>
-        </NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Profile" component={Profile} />
+            <Tab.Screen 
+                    name="Add" 
+                    component={View} 
+                    listeners={{
+                        tabPress: e => {
+                          // Prevent default action
+                          e.preventDefault();
+                          console.log("Add");
+                        },
+                      }}
+            />
+            <Tab.Screen name="Search" component={Search} />
+            <Tab.Screen name="Notifications" component={Notifications} />
+        </Tab.Navigator>
+    </NavigationContainer>
+    );
+};
 
-);
-
-
-export const makeAppContainer = createAppContainer(TabNavigation);
+//export default createAppContainer(TabNavigation);
+       
