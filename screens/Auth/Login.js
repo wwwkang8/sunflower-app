1d0ca73821de0e063169a4d52c82e6f3faa8d25f
wwwkang8 <wwwkang8@gmail.com>
2021-02-22 23:26:18 +0900

    #12.1 Auth Components part One

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import AuthButton from "../../components/AuthButton";
+import AuthInput from "../../components/AuthInput";
 
 /** View 컴포넌트의 스타일 지정 */
 const View = styled.View`
@@ -15,7 +17,8 @@ const Text = styled.Text`
 
 export default () => (
     <View>
-        <Text>Log In</Text>
+        <AuthInput value="" placeholder="Email" keyboardType="email-address" />
+        <AuthButton onPress={()=> null} text="Log In" />
     </View>
 );
 
