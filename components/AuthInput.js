5837a37ab4a9dad754f7b5a13612cb2a173b8596
wwwkang8 <wwwkang8@gmail.com>
2021-03-01 12:22:04 +0900

    #12.2 Auth Component part two

--- components/AuthInput.js
+++ components/AuthInput.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import constants from "../constants";
 
 const Container = styled.View`
     margin-bottom: 10px;
 `;
 
 const TextInput = styled.TextInput`
-    
+    background-color: ${props => props.theme.greyColor}
+    border: 1px solid ${props => props.theme.darkGreyColor}
+    border-radius: 4px;
+    width: ${constants.width / 2};
+    padding: 10px;
 `;
 
 const AuthInput = ({ placeholder
                     , value
                     , keyboardType="default"
-                    , autoCapitalize="none" }) => (
-    <Container>
-        <TextInput keyboardType={keyboardType} 
-                   placeholder={placeholder}
-                   autoCapitalize={autoCapitalize}
-                   value={value}
-                   />
-    </Container>
-);
+                    , autoCapitalize="none"
+                    , onChange }) => {
+
+
+    return(
+        <Container>
+            <TextInput 
+                    keyboardType={keyboardType} 
+                    placeholder={placeholder}
+                    autoCapitalize={autoCapitalize}
+                    value={value}
+                    onChangeText={onChange}
+                    />
+        </Container>
+    );
+};
 
 AuthInput.propTypes = {
     placeholder: PropTypes.string.isRequired,
@@ -34,7 +46,8 @@ AuthInput.propTypes = {
         "email-address",
         "phone-pad"
     ]),
-    autoCapitalize: PropTypes.oneOf(["none", "sentences", "words", "characters"])
+    autoCapitalize: PropTypes.oneOf(["none", "sentences", "words", "characters"]),
+    onChange: PropTypes.func.isRequired
 };
 
 export default AuthInput;
