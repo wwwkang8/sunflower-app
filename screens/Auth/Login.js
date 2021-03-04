1592375177e6639aaccbd440a32a94354ca5da91
wwwkang8 <wwwkang8@gmail.com>
2021-03-04 23:12:10 +0900

    #12.4 Login part Two

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -7,6 +7,8 @@ import AuthInput from "../../components/AuthInput";
 import { useState } from "react";
 import useInput from "../../hooks/useInput";
 import { Alert } from "react-native";
+import { LOG_IN } from "./AuthQueries";
+import { useMutation } from "react-apollo-hooks";
 
 /** View 컴포넌트의 스타일 지정 */
 const View = styled.View`
@@ -21,8 +23,15 @@ const Text = styled.Text`
 
 export default () => {
     const emailInput = useInput("");
+    const [loading, setLoading] = useState(false);
+    const requestSecret = useMutation(LOG_IN, {
+        variables: {
+            email: emailInput.value
+        }
+    });
 
-    const handleLogin = () => {
+
+    const handleLogin = async ({navigation}) => {
         const { value } = emailInput;
         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -32,6 +41,17 @@ export default () => {
             return Alert.alert("Please write an email");
         }else if(!emailRegex.test(value)){
             return Alert.alert("This email is invalid");
+        }
+
+        try{
+            setLoading(true);
+            await requestSecret();
+            Alert.alert("Check your email box");
+            navigation.navigate("Confirm");
+        }catch(e){
+            Alert.alert("Can't log in now");
+        }finally{
+            setLoading(false);
         }
     }
 
@@ -45,7 +65,7 @@ export default () => {
                         returnKeyType="send"
                         onEndEditing={handleLogin}
                         autoCorrect={false} />
-                <AuthButton onPress={handleLogin} text="Log In" />
+                <AuthButton onPress={handleLogin} text="Log In" loading={loading} />
             </View>
         </TouchableWithoutFeedback>
     )
