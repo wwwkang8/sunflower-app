e2eb185e310417e16f4ae0497bffa7550a18fcae
wwwkang8 <wwwkang8@gmail.com>
2021-04-05 23:56:08 +0900

    #14.0 Apollo Context

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
+import Loader from "../../components/Loader";
+import { useQuery } from 'react-apollo-hooks';
 
 const View = styled.View`
   justify-content: center;
@@ -7,10 +10,43 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
+const Text = styled.Text`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
 
-export default () => (
-  <View>
-    <Text>Home</Text>
-  </View>
-);
+const FEED_QUERY = gql`
+  {
+    seedFeed{
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        userName
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          userName
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
+export default () => {
+  const { loading, data } = useQuery(FEED_QUERY);
+  return <View>{loading ? <Loader /> : null }</View>
+};
